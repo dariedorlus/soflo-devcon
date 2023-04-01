05ad9d746fc9522b620441d0a55101cdd66c605a
Damian Montero <photoca@hotmail.com>
2023-04-01 19:59:41 -0400

    Parsing the .csv file

--- api/src/talks.js
+++ api/src/talks.js
@@ -1,13 +1,12 @@
 
 import { FieldValue } from "firebase-admin/firestore"
-import dbConnect from "./dbConnect.js"
-const TalksCollection = 'Talks'
+import {dbConnect,Collections} from "./dbConnect.js"
 const db = dbConnect()
 
 
 // Get All
 export async function getAllTalks(req, res) {
-  const documents = await db.collection(TalksCollection).get() //.orderBy('createdAt', 'desc').get()
+  const documents = await db.collection(Collections.Sessions).get() //.orderBy('createdAt', 'desc').get()
   const talks = documents.docs.map(doc => ({ ...doc.data(), restId: doc.id }));
   res.send(talks)
 }
