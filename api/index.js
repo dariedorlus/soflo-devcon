43e9219a91e1977d534f33a1b50dbbb994a51b99
Tda <tdorluss@gmail.com>
2023-04-08 14:44:34 -0400

    file rename

--- api/index.js
+++ api/index.js
@@ -1,36 +1,38 @@
-import functions from "firebase-functions";
-import express from "express";
-import cors from "cors"
-import {getAllSessions,getConferenceData,getAllTracks, getAllTimes} from "./src/Conference.js"
+import functions from 'firebase-functions';
+import express from 'express';
+import cors from 'cors';
+import {
+  getAllSessions,
+  getConferenceData,
+  getAllTracks,
+  getAllTimes,
+} from './src/monference.js';
 
 const app = express();
 app.use(cors());
 
-app.get("/test", (req,res)=>{
-    res.send("The leads api es connected");
+app.get('/test', (req, res) => {
+  res.send('The leads api es connected');
 });
 
 //API points for talks
-app.get("/api/conference/:conferenceID", getConferenceData)
-app.get("/conference/:conferenceID", getConferenceData)
+app.get('/api/conference/:conferenceID', getConferenceData);
+app.get('/conference/:conferenceID', getConferenceData);
 
+app.get('/api/sessions/:conferenceID', getAllSessions);
+app.get('/sessions/:conferenceID', getAllSessions);
 
-app.get("/api/sessions/:conferenceID", getAllSessions)
-app.get("/sessions/:conferenceID", getAllSessions)
+app.get('/api/times/:conferenceID', getAllTimes);
+app.get('/times/:conferenceID', getAllTimes);
 
-app.get("/api/times/:conferenceID", getAllTimes)
-app.get("/times/:conferenceID", getAllTimes)
+app.get('/api/tracks/:conferenceID', getAllTracks);
+app.get('/tracks/:conferenceID', getAllTracks);
 
-app.get("/api/tracks/:conferenceID", getAllTracks)
-app.get("/tracks/:conferenceID", getAllTracks)
+app.get('/', (req, res) => {
+  res.send('Nope...');
+});
+app.get('/api', (req, res) => {
+  res.send('Nope...');
+});
 
-
-
-app.get("/",(req,res)=> {
-    res.send("Nope...")
-})
-app.get("/api",(req,res)=> {
-    res.send("Nope...")
-})
-
-export const api= functions.https.onRequest(app)
+export const api = functions.https.onRequest(app);
