ad8e4d47d463a90879b0c3ee3504db77a71a27ea
Damian Montero <photoca@hotmail.com>
2023-04-10 14:35:59 -0400

    Fixing the issue of the system not selecting the next session time matching "now"

--- client/src/pages/sessionsPage.js
+++ client/src/pages/sessionsPage.js
@@ -10,17 +10,26 @@ function SessionsPage() {
   const [activeFilter, setActiveFilter] = useState('Artificial Intelligence');
 
 
+  function DateToMilitaryTime(date) {
+    var CurrentFirstTime =  date.toLocaleString('en-US', {hour12: false,})
+    CurrentFirstTime = CurrentFirstTime.substring(CurrentFirstTime.indexOf(", ")+2)
+    CurrentFirstTime = CurrentFirstTime.substr(0,CurrentFirstTime.indexOf(":"))
+    return Number(CurrentFirstTime);
+  }
+
   useEffect(() => {
     const getData = async () => {
-      const ConferenceData = await(await fetch(`${process.env.REACT_APP_API_URL}/conference/1`)).json()
+      const ConferenceData = await(await fetch(`${process.env.REACT_APP_API_URL}/conferences/1`)).json()
       setConferenceData(ConferenceData);
       // find that's the next (or current) session
       let CurrentTimeSlot = "08:00"
       var today = new Date();
-      var CurrentFirstTime =  Number(today.getHours())
+      var CurrentFirstTime =  DateToMilitaryTime(today)
+      console.log("now is: ",CurrentFirstTime)
       ConferenceData.times.map(timeSlot => {
-        const firstPartOfTime = Number(timeSlot.time.substr(0,timeSlot.time.indexOf(":")))
-        
+        let  firstPartOfTime = Number(timeSlot.time.substr(0,timeSlot.time.indexOf(":")))
+        firstPartOfTime = timeSlot.time.indexOf("AM") > -1 ? firstPartOfTime : firstPartOfTime + 12
+
         if (firstPartOfTime <= CurrentFirstTime) {
           console.log("Yep, ",firstPartOfTime,CurrentFirstTime)
           CurrentTimeSlot = timeSlot.time;
