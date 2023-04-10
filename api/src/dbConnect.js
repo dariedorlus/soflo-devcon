609993e8e5468951bb33632711d8d10f923af19c
Darie Dorlus <5068919+dariedorlus@users.noreply.github.com>
2023-04-10 13:48:55 -0400

    Code clean up and db field, and file rename rename (#3)
    * file rename
    
    * general clean up and renaming
    
    ---------
    
    Co-authored-by: Damian Montero <thedamian@users.noreply.github.com>

--- api/src/dbConnect.js
+++ api/src/dbConnect.js
@@ -1,26 +1,23 @@
-import { initializeApp, cert, getApps } from "firebase-admin/app"
-import { getFirestore } from "firebase-admin/firestore"
-import {service_account} from "./secrets.js"
+import { initializeApp, cert, getApps } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import { service_account } from './secrets.js';
 
 export const Collections = {
-  Sessions: "Sessions",
-  Conferences: "Conferences",
-  Times: "Times",
-  Tracks: "Tracks",
-  Speakers: "Speakers",
-}
-
-
+  Sessions: 'Sessions',
+  Conferences: 'Conferences',
+  Times: 'Times',
+  Tracks: 'Tracks',
+  Speakers: 'Speakers',
+};
 
 export function dbConnect() {
   // check if NOT connected
-  if(!getApps().length) {
+  if (!getApps().length) {
     // connect
     initializeApp({
-      credential: cert(service_account)
-    })
+      credential: cert(service_account),
+    });
   }
   // return db-connection
-  return getFirestore()
-  
+  return getFirestore();
 }
